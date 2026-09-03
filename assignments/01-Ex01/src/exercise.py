c70fec1787a1f8df1973f067f036735bddeeb53c
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2026-09-03 15:12:05 +0000

    Corrigiendo triangulos

--- assignments/01-Ex01/src/exercise.py
+++ assignments/01-Ex01/src/exercise.py
@@ -13,13 +13,13 @@ def main():
     El triángulo equilátero tiene 3 lados iguales, el isósceles tiene 2 lados iguales y el escaleno tiene los 3 lados diferentes.
     """
 
-    x = int(input("Ingres el primer lado del triángulo: "))
-    y = int(input("Ingres el segundo lado del triángulo: "))
-    z = int(input("Ingres el tercer lado del triángulo: "))
+    x = int(input("Ingrese el primer lado del triángulo: "))
+    y = int(input("Ingrese el segundo lado del triángulo: "))
+    z = int(input("Ingrese el tercer lado del triángulo: "))
 
     if not(x > 0 and y > 0 and z > 0):
         print("NO ES UN TRIÁNGULO")
-    elif x + y > z and x + z > y and y + z > x:
+    elif x + y > z or x + z > y or y + z > x:
         if x == y and y == z:
             print("EQUILÁTERO")
         elif x == y or x == z or y == z:
